bab4c66655f4100aa779e0a16f94682635018a14
Mohamed Ashref Ben Abdallah <109633107+Black-Spear@users.noreply.github.com>
2023-08-01 20:12:56 +0100

    Update SplineSection.tsx

--- src/Landing/SplineSection/SplineSection.tsx
+++ src/Landing/SplineSection/SplineSection.tsx
@@ -2,14 +2,14 @@ import Spline from "@splinetool/react-spline";
 
 const SplineSection = () => {
   return (
-    <div className="hidden md:block" >
+    <div className="hidden">
       <h1 className="flex items-center justify-center text-center font-display text-4xl font-medium">
         Elegance en simplicité.
       </h1>
       <div className="image-placeholder min-h-[max(550px,90vh)]">
         <Spline scene="https://prod.spline.design/UPCb0c4RpNVr2AsX/scene.splinecode" />
       </div>
-    <div/>
+    </div>
   );
 };
 
